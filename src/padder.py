cfe10f6de20890465cc694d6e4abc07e739126ff
MrVane <61145921+Mr-Vane@users.noreply.github.com>
2025-11-03 16:02:39 +0100

    Update padder.py

--- src/padder.py
+++ src/padder.py
@@ -2,6 +2,9 @@ import csv
 import ast
 import numpy as np
 import os
+from tqdm import tqdm
+
+lista_completa = []
 
 def calcular_longitudes_optimas(columna1, columna2):
     longitudes_entrada = []
@@ -31,7 +34,8 @@ def aplicar_padding(max_entrada, max_salida,columna1, columna2):
     with open("data/database/dataset_tokenizado.csv", newline="", encoding="utf-8") as dataset:
         reader = csv.DictReader(dataset)
         
-        for i, row in enumerate(reader):
+        for i, row in enumerate(tqdm(reader, desc="Padding")):
+            lista_momentanea = []
             entrada = ast.literal_eval(row[columna1])
             salida = ast.literal_eval(row[columna2])
             
@@ -45,11 +49,17 @@ def aplicar_padding(max_entrada, max_salida,columna1, columna2):
                 salida = salida + [0] * (max_salida - len(salida))  # padding
             else:
                 salida = salida[:max_salida]
+
+            lista_momentanea.append(entrada)
+            lista_momentanea.append(salida)
+
+            lista_completa.append(lista_momentanea)
             
-            # Escribir al archivo
-            file_exists = os.path.isfile("data/database/dataset_tokenizado_padded.csv")
-            with open("data/database/dataset_tokenizado_padded.csv", "a", newline="", encoding="utf-8") as f:
-                writer = csv.writer(f)
-                if not file_exists:
-                    writer.writerow([columna1, columna2])
-                writer.writerow([entrada, salida])
+        # Escribir al archivo
+        file_exists = os.path.isfile("data/database/dataset_tokenizado_padded.csv")
+        with open("data/database/dataset_tokenizado_padded.csv", "a", newline="", encoding="utf-8") as f:
+            writer = csv.writer(f)
+            if not file_exists:
+                writer.writerow([columna1, columna2])
+                for fila in lista_completa:
+                    writer.writerow([fila[0], fila[1]])
