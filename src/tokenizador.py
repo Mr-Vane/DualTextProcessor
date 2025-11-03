f43bdcf63954bad0a5360a61be5850f44e7600b4
MrVane <61145921+Mr-Vane@users.noreply.github.com>
2025-11-03 16:02:52 +0100

    Update tokenizador.py

--- src/tokenizador.py
+++ src/tokenizador.py
@@ -2,8 +2,13 @@ import csv
 import re
 import os
 import json
+from tqdm import tqdm
+import time
 
 tokens = {}
+lista_linea_token = []
+
+inicio = time.perf_counter()
 
 SPECIAL_TOKENS = {
     '<pad>': 0,
@@ -12,15 +17,17 @@ SPECIAL_TOKENS = {
     '<unk>': 3
 }
 
-def escribir_dataset(linea_entrada, linea_salida, columna1, columna2):
+def escribir_dataset(lista_linea_token, columna1, columna2):
+    print(f"[INFO] Escribiendo dataset tokenizado en data/database/dataset_tokenizado.csv")
+    print(f"[INFO] Escribiendo vocabulario en data/tokens/tokens.json y data/tokens/token_to_word.json")
     file_exists = os.path.isfile("data/database/dataset_tokenizado.csv")
     
-    with open("data/database/dataset_tokenizado.csv", "a", newline="", encoding="utf-8") as f:
+    with open("data/database/dataset_tokenizado.csv", "a"   , newline="", encoding="utf-8") as f:
         writer = csv.writer(f)
         if not file_exists:
             writer.writerow([columna1, columna2])
-        writer.writerow([linea_entrada, linea_salida])
-
+        for fila in lista_linea_token:
+            writer.writerow([str(fila[0]), str(fila[1])])
 
     with open("data/tokens/tokens.json", "w", encoding="utf-8") as f:
         json.dump(tokens, f, ensure_ascii=False, indent=2)
@@ -36,7 +43,8 @@ indice_token = len(SPECIAL_TOKENS)
 def tokenizar(dataset_name, indice_token, columna1, columna2):
     with open(dataset_name, newline="", encoding="utf-8") as dataset:
         reader = csv.DictReader(dataset)
-        for row in reader:
+        for row in tqdm(reader, desc="Tokenizando", unit="línea"):
+            lista_linea_actual = []
             entrada = row[columna1].strip()
             salida = row[columna2].strip()
 
@@ -61,5 +69,10 @@ def tokenizar(dataset_name, indice_token, columna1, columna2):
             # Añadir token de fin de secuencia
             linea_token_entrada.append(SPECIAL_TOKENS['<eos>'])
             linea_token_salida.append(SPECIAL_TOKENS['<eos>'])
-            
-            escribir_dataset(linea_token_entrada, linea_token_salida, columna1, columna2)
+
+            lista_linea_actual.append(linea_token_entrada)
+            lista_linea_actual.append(linea_token_salida)
+
+            lista_linea_token.append(lista_linea_actual)
+        
+        escribir_dataset(lista_linea_token, columna1, columna2)
