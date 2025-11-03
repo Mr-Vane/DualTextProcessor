e8d5c695f639ce8e2fb0b461830e93f46d5c44f1
MrVane <61145921+Mr-Vane@users.noreply.github.com>
2025-11-03 16:02:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import sys
+import time
 
 from src.tokenizador import tokenizar
 from src.name import leer_columnas_csv
 import src.padder
+
+inicio = time.perf_counter()
 
 tokens = {}
 indice_token = 1
@@ -26,18 +29,22 @@ def main():
 
     #Leer primera row
     columna1, columna2 = leer_columnas_csv(dataset_name)
-        
+    
+    print(f"[INFO] Iniciando tokenizacion del archivo: {dataset_name}")
+    print(f"[INFO] Columnas a tokenizar: {columna1}, {columna2}")
     #Tokenizamos database
-    print("TOKENIZAZADOR")
     tokenizar(dataset_name, indice_token, columna1, columna2)
 
     #Calculamos padding
-    print("PADDER")
+    print("[INFO] Calculando tamaños padding...")
     max_entrada, max_salida = src.padder.calcular_longitudes_optimas(columna1, columna2)
-
+    print(f"[INFO] Tamaño {columna1}: {max_entrada} | Tamaño {columna2}: {max_salida}")
     #Ejecutamos padding
     src.padder.aplicar_padding(max_entrada, max_salida, columna1, columna2)
 
+    print(f"[TERMINADO]")
+    fin = time.perf_counter()
+    print(f"Tiempo de ejecución: {fin - inicio:.6f} segundos")
 
 if __name__ == "__main__":
     main()
